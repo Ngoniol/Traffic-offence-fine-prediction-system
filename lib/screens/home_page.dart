47d083df452d123f373872010462c0da4c3f4909
Olga Ngoni <ngoniolga@gmail.com>
2023-04-19 02:51:58 +0300

    app bar

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:project/screens/login.dart';
 import 'package:project/reusable_widgets/side_navbar.dart';
+import 'package:project/screens/notifications.dart';
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -15,14 +17,33 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: const SideNav(),
-      appBar: AppBar(title: const Text("Traffic"),),
-      body:ElevatedButton(onPressed: () {
-        FirebaseAuth.instance.signOut().then((value) {
-          print("Signed out");
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) => const Login()));
-        });
-      }, child: const Text("Logout")),
+      appBar: AppBar(title: const Text("Traffic"),
+      backgroundColor: Color(0xFF1D438C),
+      toolbarHeight: 70,
+      actions: [
+        IconButton(
+            icon: Icon(Icons.notifications),
+            onPressed: () {
+              FirebaseAuth.instance.signOut().then((value) {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const Notifications())
+                );
+              });
+            }
+        ),
+        IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () {
+              FirebaseAuth.instance.signOut().then((value) {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const Login())
+                );
+              });
+            }
+        ),
+
+        ],
+      ),
     );
   }
 }
